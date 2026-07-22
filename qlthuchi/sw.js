5c34ae0ab9cd766bab0f74ef3322cc78b02d053a
dudeyue <viet2053113@gmail.com>
2026-07-22 23:29:38 +0700

    Delete qlthuchi directory

--- qlthuchi/sw.js
+++
@@ -1,4 +0,0 @@
-const C="qlythuchi-v3.2.0",A=["./","./index.html","./style.css","./app.js","./manifest.json"];
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
-self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))])));
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))) });
